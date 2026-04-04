77a86889edf3d2f7432e607b6ec980ca0a8b04e9
ThienNguyen666 <thanban93@gmail.com>
2026-04-04 21:45:21 +0700

    fix:Add validator for email, dummy FE code

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),         // / -> Home
+  route("/login", "pages/Login.tsx"),      // /login -> Login page
+  route("/register", "pages/Register.tsx") // /register -> Register page
+] satisfies RouteConfig;
